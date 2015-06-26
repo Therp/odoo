257e19969c815f1fff1638f38e339b6f39ddf263
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-06-26 18:19:24 +0200

    [FIX] Prevent error on running tests on product module.

--- addons/product/tests/test_pricelist.py
+++ addons/product/tests/test_pricelist.py
@@ -11,7 +11,8 @@ class TestPricelist(TransactionCase):
         self.product_pricelist = self.registry('product.pricelist')
         self.uom = self.registry('product.uom')
 
-        self.usb_adapter_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_product_48')[1]
+        self.usb_adapter_id = self.ir_model_data.get_object_reference(
+            cr, uid, 'product', 'product_product_48_local')[1]
         self.datacard_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_product_46')[1]
         self.unit_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_uom_unit')[1]
         self.dozen_id = self.ir_model_data.get_object_reference(cr, uid, 'product', 'product_uom_dozen')[1]
